5bbfa95bc4b8a0e061aa89c49472fce8b0c12427
Owen Campbell <owen.campbell@empiria.co.uk>
2021-12-10 14:41:38 +0000

    Add docstrings to current projection

--- server_code/observation/projectors/current.py
+++ server_code/observation/projectors/current.py
@@ -1,5 +1,16 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2021 anvilistas
+"""A projector to maintain a data table with single 'current' record for each object.
+
+The current record would normally show the state of the most recent observation, but
+can be rewound to a previous point.
+
+The target table should be named 'current' and contain the following colums:
+
+    object_type : text
+    object_id : text
+    state : simple object
+"""
 
 from functools import partial
 
@@ -13,11 +24,16 @@ table = getattr(app_tables, projection_name)
 
 
 def _reset():
+    """Reset the projection by emptying the target table"""
     for row in table.search():
         row.delete()
 
 
 def _play(observation):
+    """Update the target record with the current state
+
+    Termination events will cause the target record to be deleted.
+    """
     row = table.get(object_id=observation["object_id"]) or table.add_row(
         object_id=observation["object_id"], object_type=observation["object_type"]
     )
@@ -28,6 +44,10 @@ def _play(observation):
 
 
 def _rewind(observation):
+    """Update the target record with the previous state
+
+    Creation events will cause the target record to be deleted.
+    """
     row = table.get(object_id=observation["object_id"]) or table.add_row(
         object_id=observation["object_id"], object_type=observation["object_type"]
     )
@@ -38,6 +58,14 @@ def _rewind(observation):
 
 
 def _spool(observations, action):
+    """Call either _play or _rewind depending on the action specified
+
+    Parameters
+    ----------
+    observations : SearchIterator over app_tables.observations
+    action : str
+        either 'play' or 'rewind'
+    """
     for observation in observations:
         actions = {"play": _play, "rewind": _rewind}
         actions[action](observation)
@@ -45,6 +73,7 @@ def _spool(observations, action):
 
 @register(projection_name)
 def projector():
+    """Factory function to create an instance of the 'current' projector"""
     return Projector(
         name=projection_name,
         player=partial(_spool, action="play"),
