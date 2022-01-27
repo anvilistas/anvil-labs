f87d7b6c9ca4affad2834092c155867eff81a44f
Owen Campbell <owen.campbell@empiria.co.uk>
2022-01-27 13:12:59 +0000

    Add client side api

--- client_code/historic/__init__.py
+++ client_code/historic/__init__.py
@@ -1,4 +1,55 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2021 anvilistas
+import anvil.server
 
 __version__ = "0.0.1"
+
+
+def create(obj, projectors=None):
+    """Save a new object and optionally play all projections
+
+    Parameters
+    ----------
+    obj : portable class instance
+    projectors : list
+        of projector names to play
+
+    Returns
+    -------
+    str
+        The uid of the object
+    """
+    return anvil.server.call(
+        "anvil_labs.historic.create", obj=obj, projectors=projectors
+    )
+
+
+def update(obj, projectors=None):
+    """Save changes to an object and optionally play all projections
+
+    Parameters
+    ----------
+    obj : portable class instance
+    projectors : list
+        of projector names to play
+
+    Returns
+    -------
+    str
+        The uid of the object
+    """
+    return anvil.server.call(
+        "anvil_labs.historic.update", obj=obj, projectors=projectors
+    )
+
+
+def delete(obj, projectors=None):
+    """Delete an object and optionally play all projections
+
+    Parameters
+    ----------
+    obj : portable class instance
+    projectors : list
+        of projector names to play
+    """
+    anvil.server.call("anvil_labs.historic.delete", obj=obj, projectors=projectors)
