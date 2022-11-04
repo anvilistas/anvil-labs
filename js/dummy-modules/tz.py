325834b94935ad8ec0fb0ce474fce3986265f54f
stu <stu@anvil.works>
2022-11-04 13:28:31 +0800

    reduce anvil.tz api in minified version

--- js/dummy-modules/tz.py
+++ js/dummy-modules/tz.py
@@ -7,14 +7,6 @@ import time
 
 class tzoffset(datetime.tzinfo):
     def __init__(self, **kwargs):
-        if (
-            len(kwargs) > 1
-            or len(kwargs) == 1
-            and "seconds" not in kwargs
-            and "hours" not in kwargs
-            and "minutes" not in kwargs
-        ):
-            raise TypeError("bad initialization")
         self._offset = datetime.timedelta(**kwargs)
 
     def utcoffset(self, dt):
@@ -26,9 +18,6 @@ class tzoffset(datetime.tzinfo):
     def tzname(self, dt):
         return None
 
-    def __repr__(self):
-        return "tzoffset(%s hours)" % (self._offset.total_seconds() / 3600)
-
 
 class tzlocal(tzoffset):
     def __init__(self):
@@ -38,16 +27,9 @@ class tzlocal(tzoffset):
             s = -time.timezone
         tzoffset.__init__(self, seconds=s)
 
-    def __repr__(self):
-        return "tzlocal(%s hour offset)" % (self._offset.total_seconds() / 3600)
-
 
 class tzutc(tzoffset):
-    def __init__(self):
-        tzoffset.__init__(self, minutes=0)
-
-    def __repr__(self):
-        return "tzutc"
+    pass
 
 
 UTC = tzutc()
