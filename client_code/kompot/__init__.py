29fed440b3c8966cccf6572e2dc6ad886d680b98
Owen Campbell <owen.campbell@tanti.org.uk>
2022-11-25 17:19:33 +0000

    Fix imports

--- client_code/kompot/__init__.py
+++ client_code/kompot/__init__.py
@@ -2,15 +2,15 @@
 # Copyright (c) 2021 anvilistas
 
 from ._batcher import batch_call  # noqa F401
-from ._register import register  # noqa F401
-from ._rpc import call, call_async, call_s, callable  # noqa F401
-from ._serialize import preserve, reconstruct, serialize  # noqa F401
 from ._persist import (  # noqa F401
     LinkedAttribute,
     RowBackedStore,
     persisted_class,
     row_backed_class,
 )
+from ._register import register  # noqa F401
+from ._rpc import call, call_async, call_s, callable  # noqa F401
+from ._serialize import preserve, reconstruct, serialize  # noqa F401
 
 __version__ = "0.0.1"
 
