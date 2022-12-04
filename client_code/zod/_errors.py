5d2751ebe13312685d87974ec7c3353750724547
stu <stu@anvil.works>
2022-12-04 20:51:35 +0800

    zod: Working on error

--- client_code/zod/_errors.py
+++ client_code/zod/_errors.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2021 anvilistas
 
 from .helpers import util
+from .helpers.parse_util import DictLike
 
 __version__ = "0.0.1"
 
@@ -28,6 +29,47 @@ ZodIssueCode = util.enum(
 )
 
 
+class FieldErrors(DictLike):
+    def __init__(self):
+        self._errors = []
+
+
 class ZodError(Exception):
     def __init__(self, issues):
         self.issues = issues
+        msg = ""
+        Exception.__init__(self, msg)
+
+    def format(self):
+        def mapper(issue):
+            return issue.get("msg", "")
+
+        field_errors = FieldErrors()
+
+        def process_error(error: ZodError):
+            for issue in error.issues:
+                code = issue.get("code", None)
+                path = issue["path"]
+
+                if code == ZodIssueCode.invalid_union:
+                    for error in issue["union_errors"]:
+                        process_error(error)
+                elif not path:
+                    field_errors._errors.append(mapper(issue))
+
+                else:
+                    curr = field_errors
+                    i = 0
+                    while i < len(path):
+                        el = path[i]
+                        terminal = i == len(path) - 1
+
+                        curr[el] = curr[el] or FieldErrors()
+                        if terminal:
+                            curr[el]._errors.append(mapper(issue))
+
+                        curr = curr[el]
+                        i += 1
+
+        process_error(self)
+        return field_errors
--- client_code/zod/helpers/parse_util.py
+++ client_code/zod/helpers/parse_util.py
@@ -13,12 +13,18 @@ class DictLike:
     def __getitem__(self, key):
         return self.__dict__[key]
 
+    def __setitem__(self, key, val):
+        self.__dict__[key] = val
+
     def keys(self):
         return self.__dict__.keys()
 
     def __repr__(self):
         items = self.__dict__.items()
         return f"{type(self).__name__}({', '.join(f'{k}={v!r}' for k,v in items)})"
+
+    def __str__(self):
+        return str(self.__dict__)
 
 
 class Common(DictLike):
