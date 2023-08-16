3d714dad8d7a5a82fc22f022441bdb79a0c9778a
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2023-08-16 19:40:25 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2021 anvilistas
 
+from .. import client_unittest
+from ..UnitTestComponent import UnitTestComponent
 from ._anvil_designer import Form1Template
 
 __version__ = "0.0.1"
@@ -9,3 +11,12 @@ __version__ = "0.0.1"
 class Form1(Form1Template):
     def __init__(self, **properties):
         self.init_components(**properties)
+        self.add_component(
+            UnitTestComponent(
+                test_modules=[client_unittest],
+                card_roles=[None, None, None],
+                icon_size=30,
+                btn_role=None,
+                title_role=None,
+            )
+        )
